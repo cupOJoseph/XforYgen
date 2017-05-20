5d325ebd22d1c51a1fb551a740e6aa4f9a924b84
jschiarizzi <jschiarizzi@gwu.edu>
2017-05-20 16:32:09 -0400

    added file outlines

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # XforYgen
 A script that generates X for Y to help you come up with a start up idea.
+
+Add X to X.txt.
+Add Y to Y.txt.
+1 item per line.
---
+++ gen.py
@@ -0,0 +1,15 @@
+s = "The next big thing: "
+
+'''
+People come up to you and say, "I have the next big thing... it's X for Y"
+
+Examples include:
+- Uber for dog walkers
+- Yelp for bonsai growers
+- Snapchat for babies
+
+This generation script helps you determine what the next big thing
+is really going to be.
+'''
+
+print(s)
