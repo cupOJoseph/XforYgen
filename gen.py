38ec522d3fb098fffe6fc901fcbdc36084123a55
jschiarizzi <jschiarizzi@gwu.edu>
2017-10-13 22:50:45 -0400

    added beginner comments to prepare for workshop

--- gen.py
+++ gen.py
@@ -13,13 +13,20 @@ is really going to be.
 '''
 
 # Open files
+#Opening files in python is easy
 Xfile = open('X.txt')
 Yfile = open('Y.txt')
 
 #create lists from files
+#Here we loop through each line in the the files
+#We call string() on the lines to make it easier to read, and get rid of weird \n
+#then we add those lines to a list of lines as X and Y
 X = [line.strip() for line in Xfile]
 Y = [line.strip() for line in Yfile]
 
+#s is a string that we want to build
 s = "The next big thing is: "
+
+#get a random element in our list of lines.
 s = s + random.choice(X) + " for " + random.choice(Y)
 print (s)
